f19cf49d05f5b87cc0553e7fde0ec968b9656154
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-02-07 19:41:15 +0600

    Создание index.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
