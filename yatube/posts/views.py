41361bae5b53b90cc7f0c33841a1e9d63aa73c88
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-02-11 12:45:24 +0600

    Добавлен context во вьюшку

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
