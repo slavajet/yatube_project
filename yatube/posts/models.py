a1b83780335af8c4b4c7c7afb600ed71d341829a
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-02-12 19:45:47 +0600

    очень много всего  - надо было разбивать на этапы

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -11,4 +11,19 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+class Group(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
 def index(request):
-    template = 'posts/index.html'
-    text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': text
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)  
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': text
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context) 
